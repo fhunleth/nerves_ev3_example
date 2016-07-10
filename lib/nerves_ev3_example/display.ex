c36154182b0c41fec4a7c957f083f1a29be39b90
Frank Hunleth <fhunleth@troodon-software.com>
2016-07-09 22:21:42 -0400

    Display battery voltage

--- lib/nerves_ev3_example/display.ex
+++ lib/nerves_ev3_example/display.ex
@@ -23,6 +23,7 @@ defmodule NervesEv3Example.Display do
 
     N.mvprintw(3, 1, "IP: #{ipaddr()}")
     N.mvprintw(4, 1, "Node: #{node()}")
+    N.mvprintw(5, 1, "Battery: #{battery_voltage()}V")
     N.refresh()
     {:noreply, state + 1}
   end
@@ -31,6 +32,16 @@ defmodule NervesEv3Example.Display do
     case Nerves.NetworkInterface.settings("wlan0") do
       {:ok, settings} -> settings.ipv4_address
       _ -> "Unknown"
+    end
+  end
+
+  defp battery_voltage() do
+    case File.read("/sys/class/power_supply/legoev3-battery/voltage_now") do
+      {:ok, microvolts_str} ->
+        {microvolts, _} = Integer.parse(microvolts_str)
+        Float.round(microvolts / 1000000, 1)
+      _ ->
+        0
     end
   end
 end
